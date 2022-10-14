2019b75062eaede4568533222a256a8f1542a0aa
Dion Pham <dinhanpham310@gmail.com>
2022-10-14 10:50:00 -0700

    added console log to back app.js

--- backend/app.js
+++ backend/app.js
@@ -63,6 +63,7 @@ app.use((err, _req, _res, next) => {
         err.title = 'Validation error';
     }
 
+    console.log('this is the error----', err)
     // if (err.errors[0].includes('unique')) {
     //     err.status = 403
     // }
