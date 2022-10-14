848d561c6da8add03c0ede7aa43bf9afc9b15935
Dion Pham <dinhanpham310@gmail.com>
2022-10-14 12:11:33 -0700

    moved api docs /schema into backend directory

--- backend/app.js
+++ backend/app.js
@@ -41,12 +41,10 @@ app.use(
     })
 );
 
-
 // backend/app.js
 const routes = require('./routes');
 // ...
 app.use(routes); // Connect all the routes
-
 
 app.use((_req, _res, next) => {
     const err = new Error("The requested resource couldn't be found.");
@@ -89,6 +87,5 @@ app.use((err, _req, res, _next) => {
         });
     }
 });
-
 
 module.exports = app;
