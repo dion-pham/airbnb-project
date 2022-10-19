b52fc72980966776b00521ace47d846370cf5601
Dion Pham <dinhanpham310@gmail.com>
2022-10-19 10:35:45 -0700

    fixed spotDetail and spotReviews page for non logged in user

--- frontend/src/components/SpotDetail/index.js
+++ frontend/src/components/SpotDetail/index.js
@@ -17,49 +17,89 @@ const SpotDetail = () => {
     }, [dispatch, spotId]);
 
     const targetSpot = useSelector(state => state.spots.singleSpot)
-
     const sessionUser = useSelector(state => state.session.user)
 
     const targetSpotArray = Object.keys(targetSpot)
     if (!targetSpotArray.length) return null
 
-    if (!sessionUser) return null
+    // if (!sessionUser) return null
+
+
+    // if (sessionUser.id === targetSpot.ownerId) {
+    //     buttons = (
+    //         <div>
+    //             <button
+    //                 onClick={() => history.push(`/spots/${spotId}/edit`)}
+    //             // <Redirect to={`/spots/${spotId}/edit`}
+    //             >
+    //                 Edit this spot!
+    //             </button>
+    //         </div>
+    //     )
+    // }
+
     let buttons;
-    if (sessionUser.id === targetSpot.ownerId) {
-        buttons = (
-            <div>
-                <button
-                    onClick={() => history.push(`/spots/${spotId}/edit`)}
-                // <Redirect to={`/spots/${spotId}/edit`}
-                >
-                    Edit this spot!
-                </button>
-            </div>
-        )
+    const editSpotButton = () => {
+        if (sessionUser.id === targetSpot.ownerId) {
+            return buttons = (
+                <div>
+                    <button
+                        onClick={() => history.push(`/spots/${spotId}/edit`)}
+                    // <Redirect to={`/spots/${spotId}/edit`}
+                    >
+                        Edit this spot!
+                    </button>
+                </div>
+            )
+        }
     }
 
-    return (
-        <div className='spot-card'>
-            <h1>{targetSpot.name}</h1>
-            <h3>"{targetSpot.description}"</h3>
-            <div>{targetSpot.address}</div>
-            <div>{targetSpot.city},</div>
-            <div>{targetSpot.state}</div>
-            <div>{targetSpot.country}</div>
-            <div>Latitude: {targetSpot.lat}</div>
-            <div>Longitude: {targetSpot.lng}</div>
-            <div>Price per night: ${targetSpot.price}</div>
-            <div>
-                <img src={targetSpot?.SpotImages[0]?.url} alt="Spot's image" width="500" height="600"></img>
+    if (sessionUser) {
+        return (
+            <div className='spot-card'>
+                <h1>{targetSpot.name}</h1>
+                <h3>"{targetSpot.description}"</h3>
+                <div>{targetSpot.address}</div>
+                <div>{targetSpot.city},</div>
+                <div>{targetSpot.state}</div>
+                <div>{targetSpot.country}</div>
+                <div>Latitude: {targetSpot.lat}</div>
+                <div>Longitude: {targetSpot.lng}</div>
+                <div>Price per night: ${targetSpot.price}</div>
+                <div>
+                    <img src={targetSpot?.SpotImages[0]?.url} alt="Spot's image" width="500" height="600"></img>
+                </div>
+                {/* <div>{buttons}</div> */}
+                {editSpotButton()}
+                <div>
+                    {targetSpot.avgStarRating} - {targetSpot.numReviews} Reviews
+                </div>
+                <SpotReviews targetSpot={targetSpot} />
+                {/* <CreateReviewForm/> */}
             </div>
-            <div>{buttons}</div>
-            <div>
-                {targetSpot.avgStarRating} - {targetSpot.numReviews} Reviews
+        );
+    } else {
+        return (
+            <div className='spot-card'>
+                <h1>{targetSpot.name}</h1>
+                <h3>"{targetSpot.description}"</h3>
+                <div>{targetSpot.address}</div>
+                <div>{targetSpot.city},</div>
+                <div>{targetSpot.state}</div>
+                <div>{targetSpot.country}</div>
+                <div>Latitude: {targetSpot.lat}</div>
+                <div>Longitude: {targetSpot.lng}</div>
+                <div>Price per night: ${targetSpot.price}</div>
+                <div>
+                    <img src={targetSpot?.SpotImages[0]?.url} alt="Spot's image" width="500" height="600"></img>
+                </div>
+                <div>
+                    {targetSpot.avgStarRating} - {targetSpot.numReviews} Reviews
+                </div>
+                <SpotReviews targetSpot={targetSpot} />
             </div>
-            <SpotReviews targetSpot={targetSpot} />
-            {/* <CreateReviewForm/> */}
-        </div>
-    );
+        );
+    }
 };
 
 export default SpotDetail
