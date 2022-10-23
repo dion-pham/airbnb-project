72af96b6904643734646d1a0b3556ebecaeaa971
Dion Pham <dinhanpham310@gmail.com>
2022-10-23 02:22:21 -0700

    fixed spacing between bottom left and bottom right div in spotdetail

--- frontend/src/components/SpotDetail/index.js
+++ frontend/src/components/SpotDetail/index.js
@@ -62,26 +62,26 @@ const SpotDetail = () => {
 
             </div >
             <div className='spot-card-bottom'>
-                <div className='outer-container-spot-card-bottom-left'>
-                    <div className='spot-card-bottom-left'>
-                        {sessionUser && (<div>{editSpotButton()}</div>)}
-                        <div className='spot-card-bottom-left-hosted-by'>
-                            <h1 >Hosted by {targetSpot.Owner.firstName}</h1>
-                            3 guests · 1 bedroom · 3 beds · 1 bath
-                        </div>
-                        <div className='spot-card-bottom-left-img'>
-                            <img src='https://i.imgur.com/peICz9g.png' alt='air-cover' width='124' height='50' ></img>
-                        </div>
-                        <div className='spot-card-bottom-air-cover-info'>
-                            Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.
-                        </div>
-                        <h2>"{targetSpot.description}"</h2>
-                        <div className='spot-card-bottom-left-ratings'><i className="fa-solid fa-star"></i> {avgStarRating} · {targetSpot.numReviews} Reviews</div>
-                        <div className='spot-card-bottom-left-individual-reviews'>
-                            <SpotReviews targetSpot={targetSpot} />
-                        </div>
+                {/* <div className='outer-container-spot-card-bottom-left'> */}
+                <div className='spot-card-bottom-left'>
+                    {sessionUser && (<div>{editSpotButton()}</div>)}
+                    <div className='spot-card-bottom-left-hosted-by'>
+                        <h1 >Hosted by {targetSpot.Owner.firstName}</h1>
+                        3 guests · 1 bedroom · 3 beds · 1 bath
+                    </div>
+                    <div className='spot-card-bottom-left-img'>
+                        <img src='https://i.imgur.com/peICz9g.png' alt='air-cover' width='124' height='50' ></img>
+                    </div>
+                    <div className='spot-card-bottom-air-cover-info'>
+                        Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.
+                    </div>
+                    <h2>"{targetSpot.description}"</h2>
+                    <div className='spot-card-bottom-left-ratings'><i className="fa-solid fa-star"></i> {avgStarRating} · {targetSpot.numReviews} Reviews</div>
+                    <div className='spot-card-bottom-left-individual-reviews'>
+                        <SpotReviews targetSpot={targetSpot} />
                     </div>
                 </div>
+                {/* </div> */}
 
                 <div className='spot-card-bottom-right' >
                     <div className='container-price-rating-stars'>
